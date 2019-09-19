1e0a8aeec5ac2a09d12d3760ab23dbfac318b48c
shaban shaame <shaban@everdreamsoft.com>
2019-09-19 12:45:22 +0200

    local adaptation

--- examples/node/config.js
+++ examples/node/config.js
@@ -10,7 +10,7 @@ module.exports = {
   ROOTWETH_ADDRESS: '0x421dc9053cb4b51a7ec07b60c2bbb3ec3cfe050b',  // This is a wrapped ETH ERC20 contract address so that we can support ETH deposits to the sidechain 
   MATICWETH_ADDRESS: '0x31074c34a757a4b9FC45169C58068F43B717b2D0', // The corresponding wrapped ETH ERC20 contract address on the Matic chain 
   PRIVATE_KEY: '<paste your private key here>', // A sample private key prefix with `0x`
-  FROM_ADDRESS: '<paste address belonging to private key here>',// Your address 
+  FROM_ADDRESS: '0x7f7EED1fcBb2C2cf64d055eED1Ee051DD649C8e7',// Your address
   ROPSTEN_TEST_TOKEN: '0x70459e550254b9d3520a56ee95b78ee4f2dbd846', // Contract for ERC20 in Ropsten
   MATIC_TEST_TOKEN: '0xc82c13004c06E4c627cF2518612A55CE7a3Db699', // Contract for ERC20 in Matic testnet
   ROPSTEN_ERC721_TOKEN: '0x07d799252cf13c01f602779b4dce24f4e5b08bbd', // Contract for ERC721 in Ropsten testnet
